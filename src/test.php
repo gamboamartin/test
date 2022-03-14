7a1d2c51fa58a051029e3fb24806c76a6c7a8067
mgamboa <gamboa.martin@gmail.com>
2022-03-14 13:06:23 -0600

    v1.0

---
+++ src/test.php
@@ -0,0 +1,29 @@
+<?php
+namespace tests;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+
+
+
+class test extends TestCase{
+    public  PDO $link;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $config = new config();
+
+        $link = new PDO("mysql:host=$config->DB_HOST;dbname=$config->DB_NAME", $config->DB_USER, $config->DB_PASSWORD);
+
+        $link->query("SET NAMES 'utf8'");
+        $sql = "SET sql_mode = '';";
+        $link->query($sql);
+        $consulta = 'USE '.$config->DB_NAME;
+        $link->query($consulta);
+
+        $this->link = $link;
+    }
+
+
+}
